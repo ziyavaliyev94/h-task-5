de57c0711c5baef03a3a63d0a1d0d0be67d30942
ziyavaliyev94 <ziya.valiyev.1994@gmail.com>
2024-11-04 18:26:52 +0400

    h-task-5

---
+++ h-task-5/main.js
@@ -0,0 +1,91 @@
+// 1) Bir array yaradin adlardan ibaret olsun hemin arraya her hansisa bir adi
+// arrayin metodlarindan isdifade etmekle daxil edin daha sonra silin
+
+// let names = ['Ali', 'Veli', 'Aysel'];
+
+// names.push('Leyla');
+
+// console.log(names);
+
+// names.pop();
+
+// console.log(names);
+
+
+let obj = [
+    {
+        name: "Ali",
+        surname: "Aliyev",
+        age: 10,
+        driving: true
+    }, {
+        name: "Veli",
+        surname: "Aliyev",
+        age: 17,
+        driving: false,
+    }, {
+        name: "Mehman",
+        surname: "Aliyev",
+        age: 25,
+        driving: true,
+    }, {
+        name: "Nicat",
+        surname: "Aliyev",
+        age: 26,
+        driving: false
+    }, {
+        name: "Ayxan",
+        surname: "Aliyev",
+        age: 15,
+        driving: true
+    }, {
+        name: "Nihad",
+        surname: "Aliyev",
+        age: 19,
+        driving: true
+    }
+]
+
+
+// 2)  bu arrayde adi n ile baslayanlari loga cixaracaq funksiya qurun
+
+// function namewithn(){
+//     for(let i = 0; i < 6; i++){
+//         if(obj[i].name == "N"){
+//             console.log(obj[i]);
+
+//         }
+//     }
+// }
+// namewithn()
+
+// 3)  yasi 20 den boyuk ve 26 den kicik olan objectleri console.loga cixaran funksiya yazin
+
+// function agefilter(){
+//     for(let i = 0; i < 6; i++){
+//         if(obj[i].age > 20 && obj[i].age < 26) {
+//             console.log(obj[i]);
+
+//         }
+//     }
+// }
+// agefilter()
+
+// 4) yasi cut olan ve suruculuk vesieqesi olanlari loga cixaran funksiya yazin
+
+// function ageanddriving(){
+//     for(let i = 0; i < 6; i++){
+//         if(obj[i].age % 2 == 0 && obj[i].driving == true){
+//             console.log(obj[i]);
+
+//         }
+//     }
+// }
+// ageanddriving()
+
+// 5) obj arrayini tersine yazdirin yeni baslangic deyer Ali deyil Nihad olsun
+
+// obj.reverse();
+// console.log(obj);
+
+
